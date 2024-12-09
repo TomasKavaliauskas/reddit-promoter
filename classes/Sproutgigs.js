6f2cca63ab5db07c000b6a3f2c64b83835f47fc4
Tomas <tomas@resera.lt>
2024-12-09 20:25:18 +0200

    Finishing touches v2

--- classes/Sproutgigs.js
+++ classes/Sproutgigs.js
@@ -23,39 +23,37 @@ exports.postJob = async function (post) {
 
             }
 
-            // let response = await axios({
-            //     method: 'post',
-            //     url: 'https://sproutgigs.com/api/jobs/post-job.php',
-            //     headers: {
-            //         'Host': 'sproutgigs.com',
-            //         'Authorization': `Basic ${Buffer.from(`${env.sproutgigsUserId}:${env.sproutgigsApiKey}`).toString('base64')}`,
-            //         'Content-Type': 'application/json'
-            //     },
-            //     data: {
-            //         "zone_id": "int",
-            //         "category_id": "3790",
-            //         "title": "Upvote a post",
-            //         "instructions": [
-            //             `Go to ${post.post_link}`,
-            //             "Upvote a post"
-            //         ],
-            //         "proofs": [
-            //             {
-            //                 "description": "Reddit username you upvoted with",
-            //                 "type": "text"
-            //             }
-            //         ],
-            //         "num_tasks": post.upvotes,
-            //         "task_value": 0.04,
-            //         "speed": 1000,
-            //         "ttr": 7,
-            //         "hold_time": 15
-            //     }
-            // });
+            let response = await axios({
+                method: 'post',
+                url: 'https://sproutgigs.com/api/jobs/post-job.php',
+                headers: {
+                    'Host': 'sproutgigs.com',
+                    'Authorization': `Basic ${Buffer.from(`${env.sproutgigsUserId}:${env.sproutgigsApiKey}`).toString('base64')}`,
+                    'Content-Type': 'application/json'
+                },
+                data: {
+                    "zone_id": "int",
+                    "category_id": "3790",
+                    "title": "Upvote a post",
+                    "instructions": [
+                        `Go to ${post.post_link}`,
+                        "Upvote a post"
+                    ],
+                    "proofs": [
+                        {
+                            "description": "Reddit username you upvoted with",
+                            "type": "text"
+                        }
+                    ],
+                    "num_tasks": post.upvotes,
+                    "task_value": 0.04,
+                    "speed": 1000,
+                    "ttr": 7,
+                    "hold_time": 15
+                }
+            });
 
-            //jobId = response.data.url.split('?Id=').pop();
-
-            jobId = 'test';
+            jobId = response.data.url.split('?Id=').pop();
 
         } catch (err) {
 
